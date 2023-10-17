0c8c1b4f91f02caab0f1b7804254db18eac9e7a2
ghiri35 <ghiripdkt2017@gmail.com>
2023-10-17 12:20:55 +0530

    aq 4.1

---
+++ week4/aq-4.1/q3/app.js
@@ -0,0 +1,20 @@
+const a = new Vue({
+    el: '#app1',
+    data :{
+        message : 'kumar',
+    },
+})
+
+const b = new Vue({
+    el: '#app2',
+    data :{
+        message : 'rajput',
+    },
+})
+
+const c = new Vue({
+    el: '#app3',
+    data :{
+        message : 'abhishek',
+    },
+})
---
+++ week4/aq-4.1/q9/app.js
@@ -0,0 +1,11 @@
+const style1 = {
+    color:'red',
+}
+const style2 = {
+    fontWeight:'bold',
+}
+const optObject = {
+    el:'#app',
+    data : {style1:style1,style2:style2},
+}
+const app = new Vue(optObject)
